a2865f90bf1b1d4426bf6e966e1eead77a6b7354
Jlestat <jlestatt@gmail.com>
2023-11-22 21:08:01 +0300

    add views func

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import test
+from .views import *
 
 
 urlpatterns = [
-    path('', test, name='test'),
+    path('', index, name='home'),
 ]
--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def test(request):
+def index(request):
     return HttpResponse('Hello')
