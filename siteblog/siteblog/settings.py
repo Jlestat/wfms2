88f307a2ca0cfac4604b8b772df57e9200f24b7a
Jlestat <jlestatt@gmail.com>
2023-11-23 20:40:23 +0300

    add static files

--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'siteblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'siteblog/static'),
 ]
 
 # Default primary key field type
